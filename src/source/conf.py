d318079d41ca1f022b367cf64286145da10969b0
Sebastian Heuer <sebastian@phpeople.de>
2017-04-01 14:57:30 +0200

    remove permalinks

--- src/source/conf.py
+++ src/source/conf.py
@@ -129,6 +129,8 @@ html_theme = 'phpunit_multi_html'
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ['_templates']
 
+html_add_permalinks = None
+
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
 #html_title = None
