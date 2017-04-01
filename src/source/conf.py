52a60d4aa33048f12d11eb5dc5831cd72bc5a5d8
Sebastian Heuer <sebastian@phpeople.de>
2017-04-01 17:07:01 +0200

    use correct type when setting html_add_permalinks

--- src/source/conf.py
+++ src/source/conf.py
@@ -129,7 +129,7 @@ html_theme = 'phpunit_multi_html'
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ['_templates']
 
-html_add_permalinks = None
+html_add_permalinks = ""
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
