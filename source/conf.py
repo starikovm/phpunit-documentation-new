094bf479ba312f03dd8aae905a17653bd3c4d022
rorishko <roman.orishko@trivago.com>
2017-10-14 14:41:25 +0200

    Added Read the Docs html theme Removed phpunit_multi theme

--- source/conf.py
+++ source/conf.py
@@ -133,7 +133,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'phpunit_multi_html'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -141,7 +141,7 @@ html_theme = 'phpunit_multi_html'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ['_templates']
+#html_theme_path = ['_templates']
 
 html_add_permalinks = ""
 
@@ -319,4 +319,8 @@ numfig_format = {
     'section': 'Section'
 }
 
+
+# -- Options for epub output -------------------------------------------
+
+# A dictionary of options that influence the look and feel of the selected theme. These are theme-specific.
 epub_theme = "phpunit_epub"
